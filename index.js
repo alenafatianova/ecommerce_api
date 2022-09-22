bb981e121e87f38592ece40c9a7f8b6ea5335111
Alena Fatianova <ravemybody@gmail.com>
2022-09-22 20:17:04 +0200

    master Add get/post methods for users

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello')
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+const userRoutes = require('./routes/users')
+
+dotenv.config()
+
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGO_DB)
+.then(() => {console.log("DBConnection succesfully!")})
+.catch((error) => {
+    console.log(error)
+    throw new Error()
+})
+
+app.use(express.json())
+app.use('/api/users', userRoutes)
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Backend server is running!")
+})
