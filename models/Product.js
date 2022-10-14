457cb6ec8c76fa237d933cdb0082eb46f0f0ace5
Alena Fatianova <ravemybody@gmail.com>
2022-10-14 21:01:32 +0200

    master Create Cart schema, edit Product schema

---
+++ models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const { boolean } = require("webidl-conversions")
+
+const ProductSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true, unique: true },
+        description: { type: String, required: true, unique: true },
+        categories: { type: Array },
+        price: { type: Number, required: true },
+        size: { type: String, required: true },
+        image: { type: String, required: true },
+        image: { type: String, required: true },
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model('Product', ProductSchema)
