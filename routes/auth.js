7b432794eef08ed3fee8f6181c4914f7e30a5713
Alena Fatianova <ravemybody@gmail.com>
2022-10-15 23:09:10 +0200

    feature/crypto-auth Crypted password of user, install crypto-js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User")
-
 const router = require("express").Router()
+const cryptoJS = require("crypto-js")
 
 
 // REGISTER
@@ -8,7 +8,7 @@ router.post("/signup", async (req, res) => {
 const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: cryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET).toString(),
     })
     
     try {
